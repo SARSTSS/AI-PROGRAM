57b3df66b18d0e974cfb259741eae886cfa14020
Darkwebhacks <pillssdarknet@gmail.com>
2022-01-17 21:04:07 +0530

    AI PROGRAM

---
+++ AI PROGRAM.py
@@ -0,0 +1,15 @@
+import flask 
+
+print("AI: What is your name")
+name = input("You: ")
+print("heloo"+name)
+
+
+print("AI: What is your age")
+age = input("You: ")
+print("OK"+age)
+
+
+print("AI: What is your Gmail")
+Gmail = input("You: ")
+print("OK"+Gmail)
