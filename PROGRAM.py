f310c7dfc1d344932ca1709f8a66364c84293466
RIKHARI-GAMER <97836804+SARSTSS@users.noreply.github.com>
2022-01-17 21:41:26 +0530

    this is AI PROGRAM
    This is very nice ..

--- PROGRAM.py
+++ PROGRAM.py
@@ -2,14 +2,17 @@
 
 print("AI: What is your name")
 name = input("You: ")
-print("heloo"+name)
+print("            ")
+print("heloo " +name)
 
 
 print("AI: What is your age")
 name = input("You: ")
-print("OK"+name)
+print("            ")
+print("OK " +name)
 
 
 print("AI: What is your Gmail")
 name = input("You: ")
-print("OK"+name)
+print("            ")
+print("OK "  +name)
